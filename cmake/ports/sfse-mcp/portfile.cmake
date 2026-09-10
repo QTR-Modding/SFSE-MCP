732c6ffd4be6df49acf67fae0e140af5a5b05267
Ege Yilmaz <47591838+Quantumyilmaz@users.noreply.github.com>
2026-09-10 18:55:08 +0200

    Update SDK port to the install fix

--- cmake/ports/sfse-mcp/portfile.cmake
+++ cmake/ports/sfse-mcp/portfile.cmake
@@ -1,7 +1,7 @@
 vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
     URL https://github.com/QTR-Modding/SFSE-MCP.git
-    REF 54894193986178594d69f1791bcbba80b046bafb
+    REF b5d0a2814ba76e20a058891b09e97706521fcf08
     HEAD_REF main
 )
 
